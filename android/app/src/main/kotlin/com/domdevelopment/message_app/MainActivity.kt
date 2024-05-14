8f379c0bf0e513dedb56ae99bcb06f9c9a55460f
Rafael Domingues <rafael.domingues@confirm8.com>
2024-05-14 10:53:09 -0300

    feat: initial commit with firebase config

---
+++ android/app/src/main/kotlin/com/domdevelopment/message_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.domdevelopment.message_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:message_app/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const Home());
+  FirebaseFirestore.instance
+      .collection('colecao')
+      .doc('documento')
+      .set({'texto': 'test'});
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
