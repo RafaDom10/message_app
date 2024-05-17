8afed142b6f55bbc725479f7b7cda9914f81379b
Rafael Domingues <rafael.domingues@confirm8.com>
2024-05-17 12:36:17 -0300

    feat: implements logout function and app bar

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:message_app/services/google_sign_in.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -8,8 +10,36 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  void _onPressed(context) {
+    FirebaseAuth.instance.signOut();
+    signOutGoogle();
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text('Logout succeed'),
+      backgroundColor: Colors.green,
+    ));
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: Text(
+          '${user?.displayName}',
+          style: const TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.green,
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () => _onPressed(context),
+            icon: const Icon(Icons.exit_to_app_rounded),
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
   }
 }
