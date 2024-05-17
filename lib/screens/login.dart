6e30737e711d91779bba715f5436e860ddf9f3fc
Rafael Domingues <rafael.domingues@confirm8.com>
2024-05-17 10:34:08 -0300

    feat: implements login page

---
+++ lib/screens/login.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:message_app/widgets/sign_in_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              const Icon(
+                Icons.message_outlined,
+                color: Colors.green,
+                size: 150.0,
+              ),
+              const SizedBox(
+                height: 100.0,
+              ),
+              signInButton()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
