2c649a5457fe1d85e0767abcccdb326aabadcadb
Rafael Domingues <rafael.domingues@confirm8.com>
2024-05-17 09:38:14 -0300

    feat: add theme

---
+++ lib/theme/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+ThemeData appTheme() {
+  return ThemeData(
+      iconTheme: const IconThemeData(color: Colors.indigoAccent),
+      primarySwatch: Colors.green);
+}
